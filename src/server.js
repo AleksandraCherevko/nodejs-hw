6937618f3f45b710b9e089e7c368549c12368d60
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-10-02 22:25:54 +0200

    create and add notesRoutes

--- src/server.js
+++ src/server.js
@@ -6,24 +6,14 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-// import notesRoutes from './routes/notesRoutes.js';
+
+import notesRoutes from './routes/notesRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 app.use(logger);
 app.use(express.json());
 app.use(cors());
-
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
 
 app.get('/test-error', (req, res) => {
   throw new Error('Simulated server error');
