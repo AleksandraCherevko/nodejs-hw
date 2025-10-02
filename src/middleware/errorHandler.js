46cbcecf7f4cd87d2ae0c9a09936291e16ce613d
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-10-02 22:12:46 +0200

    update

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,22 @@
+// const errorHandler = (err, req, res, next) => {
+//   console.error('Error:', err.message);
+//   res.status(500).json({
+//     message: err.message,
+//   });
+// };
+
+// export default errorHandler;
+// src/middleware/errorHandler.js
+
 const errorHandler = (err, req, res, next) => {
-  console.error('Error:', err.message);
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
   res.status(500).json({
-    message: err.message,
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
   });
 };
 
